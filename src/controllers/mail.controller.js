d1e24691db2b911d3bbe92c6fd98d7e2fbb1aa45
yvillegas <villegasyanina.c@gmail.com>
2024-03-19 21:08:29 -0500

    update port smtp

--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 
 export let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 443,
+  port: 465,
   secure: true,
   auth: {
     user: process.env.SMTP_USER,
